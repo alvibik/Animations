8f6d61cc486353d47e5ae3d753380138fbb9a07e
Александр Биктеев <bikteev@mail.ru>
2021-07-28 17:23:57 +0900

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by albik on 28.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
